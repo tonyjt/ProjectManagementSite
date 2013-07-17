61be518cdc98bab3a330d05d3be778faccee3b52
tonyjt <tonyliujiangtao@gmail.com>
2013-07-18 00:16:37 +0800

    0.2.7 计划页面显示设计

---
+++ Code/PMS/UI/PMSSite/Controllers/PlanController.cs
@@ -0,0 +1,22 @@
+﻿using PMS.PMSSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PMS.PMSSite.Controllers
+{
+    [ProjectActionFilter]
+    public class PlanController : BaseController
+    {
+        //
+        // GET: /Plan/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Code/PMS/UI/PMSSite/Scripts/plan.index.js
@@ -0,0 +1,67 @@
+﻿$(function () {
+    var date = new Date();
+    var d = date.getDate();
+    var m = date.getMonth();
+    var y = date.getFullYear();
+
+    $('#calendar').fullCalendar({
+        // put your options and callbacks here
+        header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'month,agendaWeek,agendaDay'
+        },
+        editable: true,
+        firstDay: 1,
+        defaultView: 'agendaWeek',
+        events: [
+                    {
+                        title: 'All Day Event',
+                        start: new Date(y, m, 1),
+                        className: 'label'
+                    },
+                    {
+                        title: 'Long Event',
+                        start: new Date(y, m, d-5),
+                        end: new Date(y, m, d - 2),
+                        className:''
+                    },
+                    {
+                        id: 999,
+                        title: 'Repeating Event',
+                        start: new Date(y, m, d-3, 16, 0),
+                        allDay: false,
+                        className:'label-success'
+                    },
+                    {
+                        id: 999,
+                        title: 'Repeating Event',
+                        start: new Date(y, m, d+4, 16, 0),
+                        allDay: false
+                    },
+                    {
+                        title: 'Meeting',
+                        start: new Date(y, m, d, 10, 30),
+                        allDay: false
+                    },
+                    {
+                        title: 'Lunch',
+                        start: new Date(y, m, d, 12, 0),
+                        end: new Date(y, m, d, 14, 0),
+                        allDay: false
+                    },
+                    {
+                        title: 'Birthday Party',
+                        start: new Date(y, m, d+1, 19, 0),
+                        end: new Date(y, m, d+1, 22, 30),
+                        allDay: false
+                    },
+                    {
+                        title: 'Click for Google',
+                        start: new Date(y, m, 28),
+                        end: new Date(y, m, 29),
+                        url: 'http://google.com/'
+                    }
+            ]
+    })
+});
